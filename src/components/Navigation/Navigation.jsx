e18dab4724936370363a410eedd2e35f4de34c1e
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-16 13:35:19 +0300

    add Navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+import css from "./Navigation.module.css";
+
+import { NavLink } from "react-router-dom";
+
+export const Navigation = () => {
+  return (
+    <nav>
+      <NavLink to="/news">
+        <button className={css.btn}>News</button>
+      </NavLink>
+      <NavLink to="/notices">
+        <button className={css.btn}>Find Pet</button>
+      </NavLink>
+      <NavLink to="/friends">
+        <button className={css.btn}>Frinend</button>
+      </NavLink>
+    </nav>
+  );
+};
