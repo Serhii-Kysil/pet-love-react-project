7523281b55e15e4d2b180ce9ca92919f439b0c1b
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-18 17:15:16 +0300

    add Header to layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import { Logo } from "../Logo/Logo";
+import { Navigation } from "../Navigation/Navigation";
+
 import css from "./Header.module.css";
 
 export const Header = () => {
   return (
     <header className={css.header}>
       <Logo />
+      <Navigation />
     </header>
   );
 };
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { Header } from "./Header/Header";
 
 export default function Layout() {
   return (
     <>
+      <Header />
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
