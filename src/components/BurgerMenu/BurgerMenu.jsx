eb51a0623bb133d35aeee6839a9adb791a4b9617
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-19 11:55:00 +0300

    add burger close btn

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import css from "./BurgerMenu.module.css";
 
 import { RxHamburgerMenu } from "react-icons/rx";
+import { IoClose } from "react-icons/io5";
 
 export const BurgerMenu = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,9 +15,14 @@ export const BurgerMenu = ({ children }) => {
   return (
     <div className={css.burgerMenu}>
       <button className={css.burgerIcon} onClick={toggleMenu}>
-        <RxHamburgerMenu />
+        <RxHamburgerMenu className={css.icon} />
       </button>
-      {isOpen && <div className={css.menuContent}>{children}</div>}
+      {isOpen && (
+        <div className={css.menuContent}>
+          <IoClose onClick={toggleMenu} className={css.closeIcon} />
+          {children}
+        </div>
+      )}
     </div>
   );
 };
