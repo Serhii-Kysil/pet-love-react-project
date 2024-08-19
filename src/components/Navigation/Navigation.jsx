37151cfa803fef57864bc75eb67a57bce41e2905
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-19 12:16:09 +0300

    add burger menu basic styling

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,13 +6,13 @@ export const Navigation = () => {
   return (
     <nav className={css.navigation}>
       <NavLink to="/news">
-        <button className={css.btn}>News</button>
+        <button className={css.btnNews}>News</button>
       </NavLink>
       <NavLink to="/notices">
-        <button className={css.btn}>Find Pet</button>
+        <button className={css.btnFind}>Find Pet</button>
       </NavLink>
       <NavLink to="/friends">
-        <button className={css.btn}>Frinend</button>
+        <button className={css.btnFriend}>Our friends</button>
       </NavLink>
     </nav>
   );
