c7ec02c10d35248a7d335dbf132a27fc83e8c0f5
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-19 12:09:08 +0300

    add regBtns mobile styling

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,17 +1,14 @@
 import css from "./AuthNav.module.css";
 
-import { FiLogIn } from "react-icons/fi";
-
 export const AuthNav = () => {
   return (
-    <>
+    <div className={css.container}>
       <button type="button" className={css.logBtn}>
-        <FiLogIn className={css.icon} />
         Log in
       </button>
       <button type="button" className={css.regBtn}>
         Registration
       </button>
-    </>
+    </div>
   );
 };
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 export const Navigation = () => {
   return (
-    <nav>
+    <nav className={css.navigation}>
       <NavLink to="/news">
         <button className={css.btn}>News</button>
       </NavLink>
