20bf3e1aee88cde85d435f9a4fd6aa26b99a36ab
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-19 12:57:21 +0300

    add home page markup

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,8 +2,23 @@ import css from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
-    <>
-      <h1>HomePage</h1>
-    </>
+    <div className={css.homePageCont}>
+      <div className={css.mainInfoBlock}>
+        <h1 className={css.title}>
+          Take good <span className={css.span}>care</span> of your small pets
+        </h1>
+        <p className={css.info}>
+          Choosing a pet for your home is a choice that is meant to enrich your
+          life with immeasurable joy and tenderness.
+        </p>
+      </div>
+
+      <img
+        className={css.img}
+        srcSet="/main-img@2x.jpg, /main-img@3x.jpg"
+        src="/main-img@1x.jpg"
+        alt="Dog image"
+      />
+    </div>
   );
 }
