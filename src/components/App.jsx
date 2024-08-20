66a1c2f83b6b8aed3a0a878b0095e3d21c6eb931
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-20 10:55:02 +0300

    add RegisterPage img

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,8 @@ const FriendsPage = lazy(() => import("../pages/FriendsPage/FriendsPage"));
 const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
 const NewsPage = lazy(() => import("../pages/NewsPage/NewsPage"));
 const NoticesPage = lazy(() => import("../pages/NoticesPage/NoticesPage"));
+const LogInPage = lazy(() => import("../pages/LogInPage/LogInPage"));
+const RegisterPage = lazy(() => import("../pages/RegisterPage/RegisterPage"));
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           <Route path="/friends" element={<FriendsPage />} />
           <Route path="/news" element={<NewsPage />} />
           <Route path="/notices" element={<NoticesPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LogInPage />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,17 @@
+import PetBlock from "../../components/PetBlock/PetBlock";
 import css from "./RegisterPage.module.css";
 
 const RegisterPage = () => {
-  return <div className={css.registerCont}></div>;
+  return (
+    <div className={css.registerCont}>
+      <PetBlock
+        img1x={"/Cat@1x.png"}
+        img2x={"/Cat@2x.png"}
+        img3x={"/Cat@3x.png"}
+        alt={"Cat image"}
+      />
+    </div>
+  );
 };
 
 export default RegisterPage;
